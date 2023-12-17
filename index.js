5f0a59cb503484728a3f76d2cb5fd25f9b9de5c5
vhdent <vhdnet@gmail.com>
2023-12-17 23:32:09 +0330

    adding NewCard function

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-let firstCard = 10;
+let firstCard = 8;
 let secondCard = 11;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
-
 let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
+  renderGame();
+}
+
+function renderGame() {
+  sumEl.textContent = "Sum: " + sum;
+  cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
   } else if (sum === 21) {
@@ -18,4 +26,12 @@ function startGame() {
     isAlive = false;
   }
   messageEl.textContent = message;
+}
+
+function newCard() {
+  console.log("Drawing a new card from the deck");
+
+  let card = 7;
+  sum += card;
+  renderGame();
 }
