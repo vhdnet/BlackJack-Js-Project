6070e062bc9568fc091a878bb81c33233db3f872
vhdent <vhdnet@gmail.com>
2023-12-18 22:14:16 +0330

    finish Blackjack

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-let firstCard = 8;
-let secondCard = 11;
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let player = { Name: "Vahid", Chips: 145 };
+
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.Name + ": $" + player.Chips;
+
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
+}
+
 function startGame() {
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+
+  isAlive = true;
   renderGame();
 }
 
@@ -33,9 +53,10 @@ function renderGame() {
 }
 
 function newCard() {
-  console.log("Drawing a new card from the deck");
-  let card = 6;
-  sum += card;
-  cards.push(card);
-  renderGame();
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
 }
